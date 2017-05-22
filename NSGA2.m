11ed5ebb021de36fa91fece140d63b9d44ed77fe
marcos <marcos@marcos>
2017-05-22 17:07:05 -0300

    final adjustments

--- NSGA2.m
+++ NSGA2.m
@@ -9,7 +9,7 @@ varSize=[1 nVar];   % Size of Decision Variables Matrix
 varMin = -15;
 varMax = 17;
 
-MaxIt=1000;         % Maximum Number of Iterations
+MaxIt=50;         % Maximum Number of Iterations
 
 nPop=20;            % Population Size
 
@@ -37,26 +37,69 @@ for i=1:nPop
     pop(i).cost= cost_function(pop(i).x);
 end
 
-%Crossover
-pop_c = generate_population_children (pop, nPop, nCrossover, individual);
-
-%mutation
-pop_c = mutation_childrens (pop_c, nMutation);
-
-%merge
-pop = [pop; pop_c];
-
-%fast_non_dominated_sort
-[pop, F]=fast_non_dominated_sort(pop);
-
-
-%Print Fronts
-nf = numel(F);
-
-for i=1: nf
-   fprintf('Fronteira %i',i); disp(F{i})
+nit = 0;
+while nit < MaxIt
+    %Crossover
+    pop_c = generate_population_children (pop, nPop, nCrossover, individual);
+    
+    %mutation
+    pop_c = mutation_childrens (pop_c, nMutation);
+    
+    %merge
+    pop = [pop; pop_c];
+    
+    %fast_non_dominated_sort
+    [pop, F]=fast_non_dominated_sort(pop);
+    
+    
+    %Print Fronts
+%     nf = numel(F);
+%     
+%     for i=1: nf
+%         fprintf('Fronteira %i',i); disp(F{i})
+%     end
+    
+    %calculate crowding distance
+    pop = crowding_distance(pop, F);
+    
+    %main loop
+    i=1;
+    popn = [];
+    
+    np = 0;
+    nf = numel(F{1});
+    while (np + nf <= nPop)
+        
+        popn = [popn; pop([F{i}])];
+        
+        i= i + 1;
+        
+        np = numel(popn);
+        nf = numel(F{i});
+    end
+    
+    % Sort Based on Crowding Distance
+    popr = pop(F{i});
+    [~, CDSO]=sort([popr.crowdingDistance],'descend');
+    
+    popr = popr(CDSO);
+    popn=[popn; popr(1: nPop-np)];
+    
+    pop = popn;
+    
+    %fast_non_dominated_sort
+    [~, F]=fast_non_dominated_sort(pop);
+    
+    % Show Iteration Information
+    disp(['Iteration ' num2str(nit) ': Number of F1 Members = ' num2str(numel(F{1}))]);
+    
+    % Plot F1 Costs
+    figure(1);
+    plot_costs(pop);
+    pause(0.01);
+    
+    nit = nit + 1;
 end
-
 
 
 
